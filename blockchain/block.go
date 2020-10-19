9f07119e172a7ddde87aec9faa840a80a21c8456
originallyheart <1042531078@qq.com>
2020-10-19 18:11:26 +0800

    将结构体block内非byte字节切片类型的转换并拼接计算hash值

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,6 +1,8 @@
 package blockchain
 
 import (
+	"DataCertProject/util"
+	"bytes"
 	"time"
 )
 
@@ -8,18 +10,19 @@ import (
  *  区块结构体的定义
  */
 type Block struct {
-	Height    int    //区块高度
+	Height    int64    //区块高度
 	TimeStamp int64  //时间戳
 	Hash      []byte //区块的hash
 	Data      []byte // 数据
 	PrevHash  []byte //上一个区块的Hash
 	Version   string //版本号
+	Nonce     int64  //随机数,用于pow工作量证明算法计算
 }
 
 /**
  * 新建一个区块实例，并返回该区块
  */
-func NewBlock(height int, data []byte, prevHash []byte) (Block) {
+func NewBlock(height int64, data []byte, prevHash []byte) (Block) {
 	block := Block{
 		Height:    height + 1,
 		TimeStamp: time.Now().Unix(),
@@ -27,6 +30,17 @@ func NewBlock(height int, data []byte, prevHash []byte) (Block) {
 		PrevHash:  prevHash,
 		Version:   "0x01",
 	}
-	//block.Hash = util.SHA256Hash()
+	//调用util.SHA256Hash进行hash计算
+	heightBytes,_ :=util.IntToBytes(block.Height)
+	timeBytes,_ := util.IntToBytes(block.TimeStamp)
+	versionBytes := util.StringToBytes(block.Version)
+	blockBytes := bytes.Join([][]byte{
+		heightBytes,
+		timeBytes,
+		block.Data,
+		block.PrevHash,
+		versionBytes,
+	},[]byte{})
+	block.Hash = util.SHA256Hash(blockBytes)
 	return block
 }
---
+++ util/convert.go
@@ -0,0 +1,25 @@
+package util
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+/*
+*int64转换为[]byte
+ */
+func IntToBytes(num int64) ([]byte,error) {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff,binary.BigEndian,num)
+	if err != nil {
+		return nil,err
+	}
+	return buff.Bytes(),nil
+}
+
+/*
+*string字符串转换为[]byte
+ */
+func StringToBytes(st string) []byte {
+	return  []byte(st)
+}
